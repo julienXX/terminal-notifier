8ccb140d0712d0cdb9f6661f58e5ce4832009ec4
Eloy Duran <eloy.de.enige@gmail.com>
2012-07-15 15:48:44 +0200

    Improve output.

--- Terminal Notifier/AppDelegate.m
+++ Terminal Notifier/AppDelegate.m
@@ -19,32 +19,46 @@
     NSArray *args = [[NSProcessInfo processInfo] arguments];
 
     if ([args count] < 4 || [args count] > 5) {
-      printf("Usage: %s [sender PID] [sender name] [message] [activate app with bundle identifier]\n",
+      printf("Usage: %s sender-ID sender-name message [bundle-ID]\n" \
+             "\n" \
+             "    sender-ID\tA string which identifies the group the notifications belong to.\n" \
+             "             \tOld notifications with the same ID will be removed.\n" \
+             "  sender-name\tThe name of the application which is used as the notification’s title.\n" \
+             "      message\tThe message that is shown in the notification.\n" \
+             "    bundle-ID\tThe bundle identifier of the application to activate when the user\n" \
+             "             \tactivates (clicks) a notification. Defaults to `com.apple.Terminal'.\n" \
+             "\n" \
+             "When the user activates a notification, the results are logged to the system logs.\n" \
+             "Use Console.app to view these logs.\n",
              [[args[0] lastPathComponent] UTF8String]);
       exit(1);
     }
 
     NSString *bundleID = [args count] == 5 ? args[4] : @"com.apple.Terminal";
 
-    [self deliverNotificationForProcess:args[1]
-                                   name:args[2]
-                                message:args[3]
-        activateAppWithBundleIdentifier:bundleID];
+    [self deliverNotificationForSenderID:args[1]
+                                    name:args[2]
+                                 message:args[3]
+         activateAppWithBundleIdentifier:bundleID];
   }
 }
 
-- (void)deliverNotificationForProcess:(NSString *)senderPID
-                                 name:(NSString *)senderName
-                              message:(NSString *)message
-      activateAppWithBundleIdentifier:(NSString *)bundleID;
+- (void)deliverNotificationForSenderID:(NSString *)senderID
+                                  name:(NSString *)senderName
+                               message:(NSString *)message
+       activateAppWithBundleIdentifier:(NSString *)bundleID;
 {
   NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
   NSUserNotification *userNotification = nil;
 
   // First remove earlier notifications from same process.
   for (userNotification in center.deliveredNotifications) {
-    if ([userNotification.userInfo[@"senderPID"] isEqualToString:senderPID]) {
+    if ([userNotification.userInfo[@"senderID"] isEqualToString:senderID]) {
+      NSString *deliveredAt = [userNotification.actualDeliveryDate description];
+      printf("* Removing previous notification, which was delivered on: %s\n", [deliveredAt UTF8String]);
       [center removeDeliveredNotification:userNotification];
+      // There can only be one old notification.
+      break;
     }
   }
 
@@ -52,8 +66,7 @@
   userNotification = [NSUserNotification new];
   userNotification.title = senderName;
   userNotification.informativeText = message;
-  userNotification.userInfo = @{ @"senderPID":senderPID, @"bundleID":bundleID };
-  NSLog(@"Deliver notification: %@", userNotification);
+  userNotification.userInfo = @{ @"senderID":senderID, @"bundleID":bundleID };
 
   center.delegate = self;
   [center scheduleNotification:userNotification];
@@ -61,11 +74,24 @@
 
 - (void)userActivatedNotification:(NSUserNotification *)userNotification;
 {
+  NSString *senderID = userNotification.userInfo[@"senderID"];
   NSString *bundleID = userNotification.userInfo[@"bundleID"];
+
+  NSLog(@"User activated notification:");
+  NSLog(@"  sender ID: %@", senderID);
+  NSLog(@"sender name: %@", userNotification.title);
+  NSLog(@"    message: %@", userNotification.informativeText);
+  NSLog(@"  bundle ID: %@", bundleID);
+
   id app = [SBApplication applicationWithBundleIdentifier:bundleID];
-  NSLog(@"Activating app with bundle identifier `%@': %@", bundleID, app);
-  [app activate];
-  exit(0);
+  if (app) {
+    [app activate];
+    exit(0);
+
+  } else {
+    NSLog(@"Unable to find an application with the specified bundle indentifier.");
+    exit(1);
+  }
 }
 
 // TODO is this really needed?
@@ -79,6 +105,7 @@
 - (void)userNotificationCenter:(NSUserNotificationCenter *)center
         didDeliverNotification:(NSUserNotification *)userNotification;
 {
+  printf("* Notification delivered.\n");
   exit(0);
 }
 
