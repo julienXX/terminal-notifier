234a21a919445e6f44d885fcd035b30f86a0d6a3
Eloy Duran <eloy.de.enige@gmail.com>
2012-07-15 14:17:38 +0200

    Show a user notification with specified arguments.

--- Terminal Notifier/AppDelegate.h
+++ Terminal Notifier/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  Terminal Notifier
-//
-//  Created by Eloy Durán on 7/14/12.
-//  Copyright (c) 2012 Eloy Durán. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
--- Terminal Notifier/AppDelegate.m
+++ Terminal Notifier/AppDelegate.m
@@ -1,18 +1,44 @@
-//
-//  AppDelegate.m
-//  Terminal Notifier
-//
-//  Created by Eloy Durán on 7/14/12.
-//  Copyright (c) 2012 Eloy Durán. All rights reserved.
-//
-
 #import "AppDelegate.h"
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+- (void)applicationDidFinishLaunching:(NSNotification *)notification;
 {
-    // Insert code here to initialize your application
+  NSArray *args = [[NSProcessInfo processInfo] arguments];
+  NSLog(@"ARGS: %@", args);
+
+  if ([args count] < 3 || [args count] > 4) {
+    printf("Usage: %s [sender name] [message] [activate app with bundle identifier]\n", [[args[0] lastPathComponent] UTF8String]);
+    exit(1);
+  }
+
+  NSString *senderName = args[1];
+  NSString *message = args[2];
+
+  NSLog(@"SENDER: %@", senderName);
+  NSLog(@"MESSAGE: %@", message);
+
+  NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
+  center.delegate = self;
+
+  NSUserNotification *userNotification = [[NSUserNotification alloc] init];
+  userNotification.title = senderName;
+  userNotification.informativeText = message;
+  [center scheduleNotification:userNotification];
+
+  NSLog(@"%@", [center scheduledNotifications]);
+}
+
+// TODO is this needed?
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center shouldPresentNotification:(NSUserNotification *)userNotification;
+{
+  return YES;
+}
+
+// Once the notification is delivered we can exit.
+- (void)userNotificationCenter:(NSUserNotificationCenter *)center didDeliverNotification:(NSUserNotification *)userNotification;
+{
+  exit(0);
 }
 
 @end
