7e7510df96db3b12ffaeceb19354dcf062fcb8de
Eloy Durán <eloy.de.enige@gmail.com>
2012-07-25 20:37:16 +0200

    Rename sender ID to group ID and sender name to title.

--- Terminal Notifier/AppDelegate.m
+++ Terminal Notifier/AppDelegate.m
@@ -17,12 +17,12 @@
       const char *appVersion = [[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] UTF8String];
       printf("%s (%s) is a command-line tool to send OS X User Notifications.\n" \
              "\n" \
-             "Usage: %s sender-ID sender-name message [bundle-ID]\n" \
+             "Usage: %s group-ID sender-name message [bundle-ID]\n" \
              "\n" \
-             "    sender-ID\tA string which identifies the group the notifications belong to.\n" \
+             "     group-ID\tA string which identifies the group the notifications belong to.\n" \
              "             \tOld notifications with the same ID will be removed.\n" \
-             "  sender-name\tThe name of the application which is used as the notification’s title.\n" \
-             "      message\tThe message that is shown in the notification.\n" \
+             "        title\tThe notification title.\n" \
+             "      message\tThe notification message.\n" \
              "    bundle-ID\tThe bundle identifier of the application to activate when the user\n" \
              "             \tactivates (clicks) a notification. Defaults to `com.apple.Terminal'.\n" \
              "\n" \
@@ -34,37 +34,36 @@
 
     NSString *bundleID = [args count] == 5 ? args[4] : @"com.apple.Terminal";
 
-    [self deliverNotificationForSenderID:args[1]
-                                    name:args[2]
-                                 message:args[3]
-         activateAppWithBundleIdentifier:bundleID];
+    [self deliverNotificationForGroupID:args[1]
+                                  title:args[2]
+                                message:args[3]
+        activateAppWithBundleIdentifier:bundleID];
   }
 }
 
-- (void)deliverNotificationForSenderID:(NSString *)senderID
-                                  name:(NSString *)senderName
-                               message:(NSString *)message
-       activateAppWithBundleIdentifier:(NSString *)bundleID;
+- (void)deliverNotificationForGroupID:(NSString *)groupID
+                                title:(NSString *)title
+                              message:(NSString *)message
+      activateAppWithBundleIdentifier:(NSString *)bundleID;
 {
   NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
   NSUserNotification *userNotification = nil;
 
-  // First remove earlier notifications from same process.
+  // First remove earlier notification with the same group ID.
   for (userNotification in center.deliveredNotifications) {
-    if ([userNotification.userInfo[@"senderID"] isEqualToString:senderID]) {
+    if ([userNotification.userInfo[@"groupID"] isEqualToString:groupID]) {
       NSString *deliveredAt = [userNotification.actualDeliveryDate description];
       printf("* Removing previous notification, which was delivered on: %s\n", [deliveredAt UTF8String]);
       [center removeDeliveredNotification:userNotification];
-      // There can only be one old notification.
       break;
     }
   }
 
   // Now create and deliver the new notification
   userNotification = [NSUserNotification new];
-  userNotification.title = senderName;
+  userNotification.title = title;
   userNotification.informativeText = message;
-  userNotification.userInfo = @{ @"senderID":senderID, @"bundleID":bundleID };
+  userNotification.userInfo = @{ @"groupID":groupID, @"bundleID":bundleID };
 
   center.delegate = self;
   [center scheduleNotification:userNotification];
@@ -72,14 +71,14 @@
 
 - (void)userActivatedNotification:(NSUserNotification *)userNotification;
 {
-  NSString *senderID = userNotification.userInfo[@"senderID"];
+  NSString *groupID = userNotification.userInfo[@"groupID"];
   NSString *bundleID = userNotification.userInfo[@"bundleID"];
 
   NSLog(@"User activated notification:");
-  NSLog(@"  sender ID: %@", senderID);
-  NSLog(@"sender name: %@", userNotification.title);
-  NSLog(@"    message: %@", userNotification.informativeText);
-  NSLog(@"  bundle ID: %@", bundleID);
+  NSLog(@" group ID: %@", groupID);
+  NSLog(@"    title: %@", userNotification.title);
+  NSLog(@"  message: %@", userNotification.informativeText);
+  NSLog(@"bundle ID: %@", bundleID);
 
   id app = [SBApplication applicationWithBundleIdentifier:bundleID];
   if (app) {
