868fd3150f9ee8769ec790e4c5e9fd927475cc43
Eloy Durán <eloy.de.enige@gmail.com>
2012-07-28 03:20:17 +0200

    Specify arguments with options instead and add support to execute a command when clicked.

--- Terminal Notifier/AppDelegate.m
+++ Terminal Notifier/AppDelegate.m
@@ -1,6 +1,16 @@
 #import "AppDelegate.h"
 #import <ScriptingBridge/ScriptingBridge.h>
 
+@interface NSUserDefaults (Subscript)
+@end
+
+@implementation NSUserDefaults (Subscript)
+- (id)objectForKeyedSubscript:(id)key;
+{
+  return [self objectForKey:key];
+}
+@end
+
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification;
@@ -10,14 +20,17 @@
     [self userActivatedNotification:userNotification];
 
   } else {
-    NSArray *args = [[NSProcessInfo processInfo] arguments];
+    NSUserDefaults *options = [NSUserDefaults standardUserDefaults];
 
-    if ([args count] < 4 || [args count] > 5) {
-      const char *appName = [[args[0] lastPathComponent] UTF8String];
+    NSString *message = options[@"message"];
+    NSLog(@"MESSAGE: %@", message);
+
+    if (message == nil) {
+      const char *appName = "terminal-notifier"; //[[args[0] lastPathComponent] UTF8String];
       const char *appVersion = [[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] UTF8String];
       printf("%s (%s) is a command-line tool to send OS X User Notifications.\n" \
              "\n" \
-             "Usage: %s group-ID title message [bundle-ID]\n" \
+             "Usage: %s group-ID title message --activate [bundle-ID] --execute [command]\n" \
              "\n" \
              "     group-ID\tA string which identifies the group the notifications belong to.\n" \
              "             \tOld notifications with the same ID will be removed.\n" \
@@ -32,30 +45,37 @@
       exit(1);
     }
 
-    NSString *bundleID = [args count] == 5 ? args[4] : @"com.apple.Terminal";
+    NSString *title    = options[@"title"]    ?: @"Terminal";
+    NSString *bundleID = options[@"activate"] ?: @"com.apple.Terminal";
+    NSString *groupID  = options[@"group"];
+    NSString *command  = options[@"execute"];
 
-    [self deliverNotificationForGroupID:args[1]
-                                  title:args[2]
-                                message:args[3]
-        activateAppWithBundleIdentifier:bundleID];
+    [self deliverNotificationForGroupID:groupID
+                                  title:title
+                                message:message
+        activateAppWithBundleIdentifier:bundleID
+                    executeShellCommand:command];
   }
 }
 
 - (void)deliverNotificationForGroupID:(NSString *)groupID
                                 title:(NSString *)title
                               message:(NSString *)message
-      activateAppWithBundleIdentifier:(NSString *)bundleID;
+      activateAppWithBundleIdentifier:(NSString *)bundleID
+                  executeShellCommand:(NSString *)command;
 {
   NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
   NSUserNotification *userNotification = nil;
 
   // First remove earlier notification with the same group ID.
-  for (userNotification in center.deliveredNotifications) {
-    if ([userNotification.userInfo[@"groupID"] isEqualToString:groupID]) {
-      NSString *deliveredAt = [userNotification.actualDeliveryDate description];
-      printf("* Removing previous notification, which was delivered on: %s\n", [deliveredAt UTF8String]);
-      [center removeDeliveredNotification:userNotification];
-      break;
+  if (groupID) {
+    for (userNotification in center.deliveredNotifications) {
+      if ([userNotification.userInfo[@"groupID"] isEqualToString:groupID]) {
+        NSString *deliveredAt = [userNotification.actualDeliveryDate description];
+        printf("* Removing previous notification, which was delivered on: %s\n", [deliveredAt UTF8String]);
+        [center removeDeliveredNotification:userNotification];
+        break;
+      }
     }
   }
 
@@ -63,7 +83,11 @@
   userNotification = [NSUserNotification new];
   userNotification.title = title;
   userNotification.informativeText = message;
-  userNotification.userInfo = @{ @"groupID":groupID, @"bundleID":bundleID };
+  NSMutableDictionary *info = [NSMutableDictionary dictionary];
+  if (groupID)  info[@"groupID"]  = groupID;
+  if (bundleID) info[@"bundleID"] = bundleID;
+  if (command)  info[@"command"]  = command;
+  userNotification.userInfo = info;
 
   center.delegate = self;
   [center scheduleNotification:userNotification];
@@ -73,24 +97,43 @@
 {
   [[NSUserNotificationCenter defaultUserNotificationCenter] removeDeliveredNotification:userNotification];
 
-  NSString *groupID = userNotification.userInfo[@"groupID"];
+  NSString *groupID  = userNotification.userInfo[@"groupID"];
   NSString *bundleID = userNotification.userInfo[@"bundleID"];
+  NSString *command  = userNotification.userInfo[@"command"];
 
   NSLog(@"User activated notification:");
   NSLog(@" group ID: %@", groupID);
   NSLog(@"    title: %@", userNotification.title);
   NSLog(@"  message: %@", userNotification.informativeText);
   NSLog(@"bundle ID: %@", bundleID);
+  NSLog(@"  command: %@", command);
 
+  BOOL success = YES;
+  if (bundleID) success = [self activateAppWithBundleID:bundleID];
+  if (command)  success = [self executeShellCommand:command];
+
+  exit(success ? 0 : 1);
+}
+
+- (BOOL)activateAppWithBundleID:(NSString *)bundleID;
+{
   id app = [SBApplication applicationWithBundleIdentifier:bundleID];
   if (app) {
     [app activate];
-    exit(0);
+    return YES;
 
   } else {
     NSLog(@"Unable to find an application with the specified bundle indentifier.");
-    exit(1);
+    return NO;
   }
+}
+
+- (BOOL)executeShellCommand:(NSString *)command;
+{
+  NSTask *task = [NSTask launchedTaskWithLaunchPath:@"/bin/sh"
+                                          arguments:@[@"-c", command]];
+  [task waitUntilExit];
+  return [task terminationStatus] == 0;
 }
 
 // TODO is this really needed?
